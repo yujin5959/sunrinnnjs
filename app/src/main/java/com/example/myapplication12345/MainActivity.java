5a632e0e8c9c4d30f0ae284441530aa021ebb73a
yujin5959 <jinjin17104@naver.com>
2020-08-13 01:31:51 +0900

    ddd

--- app/src/main/java/com/example/myapplication12345/MainActivity.java
+++ app/src/main/java/com/example/myapplication12345/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.myapplication12345;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,6 +47,27 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         Intent intent = new Intent(getApplicationContext(),it.class);
+                        startActivity(intent);
+                    }
+                });
+
+                Button wk1 = findViewById(R.id.sunrinwk);
+                wk1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getApplicationContext(), "선린위키에 오신것을 환영합니다", Toast.LENGTH_SHORT).show();
+                        finish();
+                       Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://sunrinwiki.layer7.kr/index.php/%EB%8C%80%EB%AC%B8"));
+                       startActivity(intent);
+                    }
+                });
+                Button bb = findViewById(R.id.bamboo);
+                bb.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getApplicationContext(), "선린대나무숲에 오신것을 환영합니다.", Toast.LENGTH_SHORT).show();
+                        finish();
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/sunrinbamboo/"));
                         startActivity(intent);
                     }
                 });
